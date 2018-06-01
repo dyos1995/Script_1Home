0c42b4fc2c47a86b6cf31cf3d78109e93673c692
dyos1995 <32826199+dyos1995@users.noreply.github.com>
2018-06-01 15:49:40 +0900

    파싱 10%
    파싱 10%

--- my-project-by-script/test.py
+++ my-project-by-script/test.py
@@ -1,19 +1,20 @@
-from tkinter import *
-from tkinter import font
-import tkinter.messagebox
-
-from urllib2 import Request, urlopen
-from urllib import urlencode, quote_plus
+from urllib import request
 from xml.etree import ElementTree
-#from mail import *
-
-key = 'HLlS60zzsxYZkqh4LVPkBQ60VtdA44bGdv65AfmojamkRl4qu1n1uaCLdJfY5GKZleapeEjVacbJgf2M23da5g%3D%3D'
-url = 'http://apis.data.go.kr/1300000/MachumTG/list'
-
-queryParams = '?' + urlencode({ quote_plus('ServiceKey') : key, quote_plus('numOfRows') : '10', quote_plus('pageNo') : '1' })
 
 
-request = Request(url + queryParams)
-request.get_method = lambda: 'GET'
-response_body = urlopen(request).read()
-print(response_body)
+url = "http://apis.data.go.kr/1300000/mjbJiWon/list?"
+row = "numOfRows=1&" #한페이지 결과수
+page = "pageNo=1&"     # 페이지 수
+key = "ServiceKey=HLlS60zzsxYZkqh4LVPkBQ60VtdA44bGdv65AfmojamkRl4qu1n1uaCLdJfY5GKZleapeEjVacbJgf2M23da5g%3D%3D"
+
+name = "gsteukgiNm"
+url_1 = url + row + page + key
+
+response = request.urlopen(url_1).read()
+tree = ElementTree.fromstring(response)
+itemElements = tree.getiterator("item")
+
+
+
+print(response)
+print(itemElements)
