fa9e630436eeafc2926dfde4a78c11887635cc34
dyos1995 <32826199+dyos1995@users.noreply.github.com>
2018-06-01 16:22:06 +0900

    파싱
    파싱 15%

--- my-project-by-script/test.py
+++ my-project-by-script/test.py
@@ -3,18 +3,24 @@ from xml.etree import ElementTree
 
 
 url = "http://apis.data.go.kr/1300000/mjbJiWon/list?"
-row = "numOfRows=1&" #한페이지 결과수
-page = "pageNo=1&"     # 페이지 수
+row = "numOfRows=10&" #한페이지 결과수
+page = "pageNo=4&"     # 페이지 수
 key = "ServiceKey=HLlS60zzsxYZkqh4LVPkBQ60VtdA44bGdv65AfmojamkRl4qu1n1uaCLdJfY5GKZleapeEjVacbJgf2M23da5g%3D%3D"
 
 name = "gsteukgiNm"
 url_1 = url + row + page + key
 
+print(url_1)
+
+search_item = input()
+print(search_item)
 response = request.urlopen(url_1).read()
 tree = ElementTree.fromstring(response)
 itemElements = tree.getiterator("item")
+for item in itemElements:
+        name_ = item.find('gtcdNm2')
+        name_ = name_.text
+        if(name_ == search_item):
+            result = item.find("gsteukgiNm")
 
-
-
-print(response)
-print(itemElements)
+print(result.text)
